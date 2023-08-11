fd32398b3953df92e89e1c6ea7e477e48509f49f
Andrew Opio <andrewaopio@gmail.com>
2023-08-11 19:58:11 -0400

    Add Image Page functionality

--- image-search/src/ImagePage.js
+++ image-search/src/ImagePage.js
@@ -1,6 +1,33 @@
+import { useLocation } from 'react-router-dom'
+function ImagePage()  {
+    const location = useLocation()
+    console.log(location.state.data)
+    return (
+    <div>
+         <h1>
+         Posted by: {location.state.data.user}
+        </h1>
+        <div>
+    
+        <img src={location.state.data.previewURL} alt="Photo Missing" width={location.state.data.previewWidth} height={location.state.data.previewHeight}/> 
+        <h4>
+        Image tags: {location.state.data.tags}
+        </h4>
 
+        <h5>
+        Likes: {location.state.data.likes}
 
-export default function ImagePage() {
-
+        </h5>
+        <h5>
+        Comments: {location.state.data.comments}
+        </h5>
+        <h5>
+        Views: {location.state.data.views}
+        </h5>
+        </div>
+    </div>
+    )
 
 }
+
+export default ImagePage;
